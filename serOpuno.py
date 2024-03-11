56d304c7ad2f2630ce1d8d10580aeb950e10a294
Sofia Cespedes <sofivampaireknight@gmail.com>
2024-03-10 20:09:47 -0500

    corregi errores

--- serOpuno.py
+++ serOpuno.py
@@ -1,17 +1,17 @@
 # servidor.py
 from concurrent import futures
 import grpc
-import suma_pb2
-import suma_pb2_grpc
+import serOPuno_pb2
+import serOPuno_pb2_grpc
 
-class SumaService(suma_pb2_grpc.SumaServiceServicer):
+class SumaService(serOPuno_pb2_grpc.SumaServiceServicer):
     def Sumar(self, request, context):
         resultado = request.numero1 + request.numero2
-        return suma_pb2.SumaResponse(resultado=resultado)
+        return serOPuno_pb2.SumaResponse(resultado=resultado)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    suma_pb2_grpc.add_SumaServiceServicer_to_server(SumaService(), server)
+    serOPuno_pb2_grpc.add_SumaServiceServicer_to_server(SumaService(), server)
     server.add_insecure_port("[::]:50051")
     server.start()
     print("Servidor RPC escuchando en el puerto 50051...")
